2c78b93b2b4146f0b74f5ff48fae502aab6279f5
j-d-han <junding98@gmail.com>
2024-07-02 13:58:11 +0800

    Added Angular skeleton to backend API

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+/*
+allow  in order to use it, add it as middleware 'https://localhost:5001/api/users' from origin 'http://localhost:4200' 
+has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.*/
+builder.Services.AddCors(); // to use it, add it as middleware. Code below HTTP request pipeline
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
@@ -16,17 +20,9 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
+// ====== Configure the HTTP request pipeline. ======
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
