765516b7f6c76f64ec70740eccb876cea75a409e
j-d-han <junding98@gmail.com>
2024-07-04 14:44:25 +0800

    Added client login and register

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  imports: [RegisterComponent],
+})
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  registerMode = false;
+  users: any;
+  http = inject(HttpClient);
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Users request completed'),
+    });
+  }
+}
